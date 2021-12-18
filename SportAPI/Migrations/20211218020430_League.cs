516e236c5d4c485bb74042ffc33bae1e2bfc056e
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2021-12-18 03:06:09 +0100

    feat: add migration with configuration for league table

---
+++ SportAPI/Migrations/20211218020430_League.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportAPI.Migrations
+{
+    public partial class League : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "LeagueId",
+                schema: "Sport",
+                table: "SportClub",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "League",
+                schema: "Sport",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(140)", maxLength: 140, nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsHigh = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "'1'"),
+                    CountForChampionsLeague = table.Column<int>(type: "int", nullable: false),
+                    CountForEuropeLeague = table.Column<int>(type: "int", nullable: false),
+                    CountForConferenceLeague = table.Column<int>(type: "int", nullable: false),
+                    CountForDownLeague = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_League", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportClub_LeagueId",
+                schema: "Sport",
+                table: "SportClub",
+                column: "LeagueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SportClub_League_LeagueId",
+                schema: "Sport",
+                table: "SportClub",
+                column: "LeagueId",
+                principalSchema: "Sport",
+                principalTable: "League",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SportClub_League_LeagueId",
+                schema: "Sport",
+                table: "SportClub");
+
+            migrationBuilder.DropTable(
+                name: "League",
+                schema: "Sport");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SportClub_LeagueId",
+                schema: "Sport",
+                table: "SportClub");
+
+            migrationBuilder.DropColumn(
+                name: "LeagueId",
+                schema: "Sport",
+                table: "SportClub");
+        }
+    }
+}
--- SportAPI/Sport/Configurations/LeagueConfiguration.cs
+++ SportAPI/Sport/Configurations/LeagueConfiguration.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportAPI.Sport.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SportAPI.Sport.Configurations
 {
-  public class LeagueConfiguration
+  public class LeagueConfiguration : IEntityTypeConfiguration<League>
   {
+    public void Configure(EntityTypeBuilder<League> builder)
+    {
+      builder.ToTable("League", "Sport");
+      builder.HasKey(e => e.Id);
+
+      builder.Property(e => e.Name)
+        .IsRequired()
+        .HasMaxLength(140);
+
+      builder.Property(e => e.CountForChampionsLeague)
+        .IsRequired().HasColumnType("int");
+
+      builder.Property(e => e.CountForEuropeLeague)
+        .IsRequired().HasColumnType("int");
+      
+      builder.Property(e => e.CountForConferenceLeague)
+        .IsRequired().HasColumnType("int");
+
+      builder.Property(e => e.CountForDownLeague)
+        .IsRequired().HasColumnType("int");
+
+      builder.Property(e => e.IsHigh)
+      .IsRequired()
+      .HasDefaultValueSql("'1'");
+    }
   }
 }
