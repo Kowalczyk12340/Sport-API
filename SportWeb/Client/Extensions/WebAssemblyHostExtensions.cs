6357041ad8d3810ef3798c04f15b8cd715cd7384
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2021-12-18 21:11:50 +0100

    feat: add mud blazor web assembly to my project

---
+++ SportWeb/Client/Extensions/WebAssemblyHostExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportWeb.Client.Extensions
+{
+  public static class WebAssemblyHostExtensions
+  {
+    public async static Task SetDefaultCulture(this WebAssemblyHost host)
+    {
+      var localStorage = host.Services.GetRequiredService<ILocalStorageService>();
+      var cultureString = await localStorage.GetItemAsync<string>("Culture");
+
+      CultureInfo cultureInfo;
+
+      if (!string.IsNullOrWhiteSpace(cultureString))
+      {
+        cultureInfo = new CultureInfo(cultureString);
+      }
+      else
+      {
+        cultureInfo = new CultureInfo("en-US");
+      }
+
+      CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+      CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+    }
+  }
+}
